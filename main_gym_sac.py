6918e506535f034f27b83a92d2191551ce125d9a
devarajuvinoda <devarajuvinoda@gmail.com>
2021-07-21 22:52:22 +0530

    main file for default gym envs added

---
+++ main_gym_sac.py
@@ -0,0 +1,56 @@
+import pybullet_envs
+import gym
+import numpy as np
+from agent import Agent
+from plot_utils import plot_learning_curve
+from gym import wrappers
+
+if __name__ == '__main__':
+    env = gym.make('HalfCheetahBulletEnv-v0')
+    agent = Agent(input_dims=env.observation_space.shape, env=env,
+            n_actions=env.action_space.shape[0])
+    n_episodes = 301
+    # uncomment this line and do a mkdir tmp && mkdir tmp/video if you want to
+    # record video of the agent playing the game.
+    env = wrappers.Monitor(env, 'tmp/video', video_callable=lambda episode_id: True, force=True)
+    filename = 'HalfCheetahBulletEnv.png'
+
+    figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+    load_chkpt = False
+
+    if load_chkpt:
+        agent.load_models()
+        env.render(mode='human')
+
+    for i in range(n_episodes):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action = agent.choose_action(observation)
+            next_observation, reward, done, info = env.step(action)
+            score += reward
+            agent.store_tuples(observation, action, reward, next_observation, done)
+            if not load_chkpt:
+                agent.learn()
+            observation = next_observation
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+#         if avg_score > best_score:
+#             best_score = avg_score
+#             if not load_chkpt:
+#                 agent.save_models()
+
+        if i%5==0 and not load_chkpt:
+            agent.save_models(i)
+
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
+
+    if not load_chkpt:
+        x = [i+1 for i in range(n_episodes)]
+        plot_learning_curve(x, score_history, figure_file)
+

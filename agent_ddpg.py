937c3465ada8ecc0d3787e625d0aa17ae48ded0a
devarajuvinoda <devarajuvinoda@gmail.com>
2021-11-13 19:43:50 +0530

    ddpg1

--- agent_ddpg.py
+++ agent_ddpg.py
@@ -104,9 +104,13 @@ class Agent:
         self.min_action = env.action_space.low[0]
         
         self.actor = ActorNetwork(n_actions=n_actions, name='actor')
-        self.critic = CriticNetwork(n_actions=n_actions, name='critic')
+        self.critic = CriticNetwork(
+            # n_actions=n_actions, 
+            name='critic')
         self.target_actor = ActorNetwork(n_actions=n_actions, name='target_actor')
-        self.target_critic = CriticNetwork(n_actions=n_actions, name='target_critic')
+        self.target_critic = CriticNetwork(
+            # n_actions=n_actions, 
+            name='target_critic')
 
         self.actor.compile(optimizer=Adam(learning_rate=alpha))
         self.critic.compile(optimizer=Adam(learning_rate=beta))
@@ -131,7 +135,7 @@ class Agent:
             weights.append(weight * tau + targets[i]*(1-tau))
         self.target_critic.set_weights(weights)
 
-    def remember(self, state, action, reward, new_state, done):
+    def store_tuples(self, state, action, reward, new_state, done):
         self.memory.store_transition(state, action, reward, new_state, done)
 
     def save_models(self):
